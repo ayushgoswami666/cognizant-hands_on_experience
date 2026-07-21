811286fc833d74d7e4e36ade5b1482d38d28b1f9
ayushgoswami666 <ayushgoswami669@gmail.com>
2026-07-21 15:07:20 +0530

    Added Mockito1

---
+++ Mockito/Mockitoexcercise1/src/main/java/com/mock/Mockitoexcercise1/ExternalApi.java
@@ -0,0 +1,6 @@
+package com.mock.Mockitoexcercise1;
+
+public interface ExternalApi {
+	 String getData();
+
+}
---
+++ Mockito/Mockitoexcercise1/src/main/java/com/mock/Mockitoexcercise1/MyService.java
@@ -0,0 +1,17 @@
+package com.mock.Mockitoexcercise1;
+
+public class MyService {
+	
+
+	    private ExternalApi api;
+
+	    public MyService(ExternalApi api) {
+	        this.api = api;
+	    }
+
+	    public String fetchData() {
+	        return api.getData();
+	    }
+	}
+
+
