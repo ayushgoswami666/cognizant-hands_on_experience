fbe24889d57db5692606ba253cdfddc8ac72d3d8
ayushgoswami666 <ayushgoswami669@gmail.com>
2026-07-21 15:10:40 +0530

    Added SL4J

---
+++ SL4J/SLJ4Excercise/src/main/java/com/slj4/SLJ4Excercise/App.java
@@ -0,0 +1,13 @@
+package com.slj4.SLJ4Excercise;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
