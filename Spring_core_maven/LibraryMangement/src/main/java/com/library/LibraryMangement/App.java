8dfc62851e965670ad472e6886948057cc12b702
ayushgoswami666 <ayushgoswami669@gmail.com>
2026-07-21 17:46:45 +0530

    Added springcore

---
+++ Spring_core_maven/LibraryMangement/src/main/java/com/library/LibraryMangement/App.java
@@ -0,0 +1,24 @@
+package com.library.LibraryMangement;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.library.services.BookService;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	ApplicationContext context =
+                new ClassPathXmlApplicationContext("Applicationconfig.xml");
+
+        BookService service =
+                context.getBean("bookService", BookService.class);
+
+        service.displayService();
+    }
+}
