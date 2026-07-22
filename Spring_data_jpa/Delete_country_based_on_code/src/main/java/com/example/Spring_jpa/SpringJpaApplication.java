cd878855c5da486fc34e55b63afa266551eaa126
ayushgoswami666 <ayushgoswami669@gmail.com>
2026-07-22 16:25:11 +0530

    Added spring-jpa-5

---
+++ Spring_data_jpa/Delete_country_based_on_code/src/main/java/com/example/Spring_jpa/SpringJpaApplication.java
@@ -0,0 +1,56 @@
+package com.example.Spring_jpa;
+
+import java.util.List;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.example.Spring_jpa.entity.Country;
+import com.example.Spring_jpa.service.CountryServices;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@SpringBootApplication
+public class SpringJpaApplication {
+	
+	   private static final Logger LOGGER =
+	            LoggerFactory.getLogger(SpringJpaApplication.class);
+
+	    private static CountryServices countryService;
+
+	public static void main(String[] args) {
+		
+		ApplicationContext context =
+                SpringApplication.run(SpringJpaApplication.class, args);
+
+        countryService = context.getBean(CountryServices.class);
+
+        LOGGER.info("Inside main");
+
+        testGetAllCountries();
+        testDeleteCountry();
+
+	}
+	
+	private static void testGetAllCountries() {
+
+        LOGGER.info("Start");
+
+        List<Country> countries =
+                countryService.getAllCountries();
+
+        LOGGER.debug("Countries = {}", countries);
+
+        LOGGER.info("End");
+
+    }
+	private static void testDeleteCountry() {
+
+	    countryService.deleteCountry("NP");
+
+	    System.out.println("Country Deleted");
+	}
+	
+
+}
---
+++ Spring_data_jpa/Delete_country_based_on_code/src/main/java/com/example/Spring_jpa/service/CountryServices.java
@@ -0,0 +1,38 @@
+package com.example.Spring_jpa.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.Spring_jpa.entity.Country;
+import com.example.Spring_jpa.repo.CountryRepository;
+
+import jakarta.transaction.Transactional;
+
+@Service
+public class CountryServices {
+
+    private final CountryRepository countryRepository;
+
+    CountryServices(CountryRepository countryRepository) {
+        this.countryRepository = countryRepository;
+    }
+ 
+    @Transactional
+    public List<Country> getAllCountries() {
+        return countryRepository.findAll();
+    }
+    
+    
+    @Transactional
+    public void deleteCountry(String code) {
+        countryRepository.deleteById(code);
+    }
+    @Transactional
+    public Country getCountry(String code) {
+        return countryRepository.findById(code).orElse(null);
+    }
+    
+
+}
