4fdea29610f0b58d9e55717ec2eac4d642e985cd
ayushgoswami666 <ayushgoswami669@gmail.com>
2026-07-27 00:10:15 +0530

    Added react_06

---
+++ React/React_06/tranieapp/src/Components/Home.js
@@ -0,0 +1,17 @@
+function Home() {
+
+    return (
+
+        <div>
+
+            <h2>Welcome to Trainers Portal</h2>
+
+            <p>Select Trainers from the menu.</p>
+
+        </div>
+
+    );
+
+}
+
+export default Home;
---
+++ React/React_06/tranieapp/src/TrainersMock.js
@@ -0,0 +1,34 @@
+import Trainer from "./Trainer";
+
+const trainers = [
+
+    new Trainer(
+        1,
+        "Aathma",
+        "aathma@gmail.com",
+        "9876543210",
+        ".NET",
+        "C#, ASP.NET, SQL Server"
+    ),
+
+    new Trainer(
+        2,
+        "Jojo Jose",
+        "jojo@gmail.com",
+        "9876543211",
+        "Java",
+        "Java, Spring Boot, Hibernate"
+    ),
+
+    new Trainer(
+        3,
+        "Elsa Smith",
+        "elsa@gmail.com",
+        "9876543212",
+        "React",
+        "React, JavaScript, HTML, CSS"
+    )
+
+];
+
+export default trainers;
