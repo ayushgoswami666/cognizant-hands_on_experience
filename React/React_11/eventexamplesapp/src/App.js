22d11033c57ae1062d563f0e3e4634655a89a40b
ayushgoswami666 <ayushgoswami669@gmail.com>
2026-07-27 14:32:40 +0530

    Added react_11

---
+++ React/React_11/eventexamplesapp/src/App.js
@@ -0,0 +1,22 @@
+import Counter from "./Counter";
+import CurrencyConvertor from "./CurrencyConvertor";
+
+function App() {
+
+  return (
+
+    <div style={{ padding: "20px" }}>
+
+      <Counter />
+
+      <hr />
+
+      <CurrencyConvertor />
+
+    </div>
+
+  );
+
+}
+
+export default App;
